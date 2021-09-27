a7e12d64f3de8d882182b5ab73eb9fb91bc9c253
Осипчук Сергей Николаевич <sergei_osipchuk@epam.com>
2021-09-27 22:15:54 +0300

    added solution for task 1.1

---
+++ modules/module_1/task1.js
@@ -0,0 +1,17 @@
+console.log('Application is running');
+
+process.stdout.write('> ');
+
+process.stdin.on('data', (data) => {
+  const inputData = data.toString().trim();
+
+  if (inputData === 'q') {
+    console.log('Application has been stopped');
+    process.exit();
+  }
+
+  const outputData = inputData.split('').reverse().join('') + '\n';
+
+  process.stdout.write(outputData);
+  process.stdout.write('> ');
+});

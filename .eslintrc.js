f2080b95321bdc8c07c0386b78dd8c53d1e27a0c
Осипчук Сергей Николаевич <sergei_osipchuk@epam.com>
2021-10-19 22:47:47 +0300

    setup eslint for typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,54 @@
 module.exports = {
+  root: true,
   env: {
     es2021: true,
-    node: true
+    node: true,
   },
   extends: [
     'eslint:recommended',
-    'airbnb-base/legacy'
+    'airbnb-base/legacy',
   ],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   rules: {
     indent: ['error', 2],
     'linebreak-style': 'off',
     quotes: ['error', 'single'],
-    semi: ['error', 'always']
-  }
+    'comma-dangle': ['error', 'always-multiline'],
+    semi: ['error', 'always'],
+  },
+  overrides: [
+    {
+      files: ['**/*.ts'],
+      env: {
+        es6: true,
+        node: true,
+      },
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+      ],
+      globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+      },
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+      plugins: ['@typescript-eslint'],
+      rules: {
+        indent: ['error', 2],
+        'linebreak-style': 'off',
+        quotes: ['error', 'single'],
+        'comma-dangle': ['error', 'always-multiline'],
+        semi: ['error', 'always'],
+      },
+    },
+  ],
 };

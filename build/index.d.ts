36b28a87768a1eee55fd473dbda6a235396075c1
Vertigo <vertigomaill@gmail.com>
2021-10-21 23:25:59 +0300

    added and setup babel

---
+++ build/index.d.ts
@@ -0,0 +1,3 @@
+declare const helloText = "Hello";
+declare const tsText = "Typescript!!";
+declare const print: (text: string) => void;
--- build/index.js
+++ build/index.js
@@ -1,3 +1,11 @@
 "use strict";
-console.log('Hello');
-console.log('Typescript!');
+
+var helloText = 'Hello';
+var tsText = 'Typescript!!';
+
+var print = function print(text) {
+  console.log(text);
+};
+
+print(helloText);
+print(tsText);
